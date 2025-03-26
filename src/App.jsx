506df3e013636805ead05c73fea9de75264d3a22
Gunjan Rathore <rathoregunjan46@gmail.com>
2025-03-26 18:30:47 +0530

    added paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <h1>Hello world -- mene change kr diya </h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus vitae atque, fuga repellendus perferendis velit? Alias eos nam optio blanditiis, veritatis tenetur dicta! Blanditiis ea nulla eius amet architecto eos!</p>
     </>
   ) 
 }
